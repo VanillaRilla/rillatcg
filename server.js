5d4a612d9532aa77ee0140c2c48587d4a4068f44
VanillaRilla <m.hertwig97@gmail.com>
2025-04-03 21:02:40 +0200

    update

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.post('/login', (req, res) => {
 
 // Start the server
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3306');
 });
