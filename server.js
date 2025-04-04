6219ee59dc15f10e7a9f7566dfad0f5d903cfcd1
VanillaRilla <m.hertwig97@gmail.com>
2025-04-04 15:40:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(session({
 passport.use(new TwitchStrategy({
     clientID: process.env.TWITCH_CLIENT_ID,
     clientSecret: process.env.TWITCH_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/twitch/callback',
+    callbackURL: 'https://d8cd-5-146-251-5.ngrok-free.app/auth/twitch/callback',
     scope: 'user:read:email' // Add more scopes as needed
 }, function (accessToken, refreshToken, profile, done) {
     return done(null, profile); // Save profile data, you can store it in the session or DB
